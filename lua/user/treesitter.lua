bc1f6981030d13ba9a4d181dfffd103aec5a5c26
orbital <orbital@holgerines.de>
2022-02-04 00:42:08 -0800

    Add protected call to treesitter load

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,4 +1,8 @@
-local configs = require("nvim-treesitter.configs")
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    return
+end
+
 configs.setup {
     ensure_installed = "maintained",
     sync_install = false,
