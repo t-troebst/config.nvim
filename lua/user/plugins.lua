35ba48257ddc91bfbee1ced1194ebe61a1a9b09f
orbital <orbital@holgerines.de>
2022-02-15 11:10:39 -0800

    Add new cmp sources

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -78,7 +78,9 @@ return packer.startup(function(use)
     use "hrsh7th/cmp-cmdline" -- Command line completions
     use "hrsh7th/cmp-nvim-lsp" -- LSP completions
     use "hrsh7th/cmp-nvim-lua" -- Lua completions
+    use "hrsh7th/cmp-nvim-lsp-signature-help" -- Display signature while typing
     use "saadparwaiz1/cmp_luasnip" -- Snippet completions
+    use "hrsh7th/cmp-cmdline" -- Command line completions
 
     -- Snippets
     use "L3MON4D3/LuaSnip" --snippet engine
