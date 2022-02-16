71ca4d00bc815151296f470dd6c8ec25f0990ffc
orbital <orbital@holgerines.de>
2022-02-16 15:42:51 -0800

    Fix bad highlighting in lsp_handlers.lua

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -3,10 +3,10 @@ local M = {}
 -- TODO: backfill this to template
 M.setup = function()
     local signs = {
-        { name = "DiagnosticSignError", text = "" },
-        { name = "DiagnosticSignWarn", text = "" },
-        { name = "DiagnosticSignHint", text = "" },
-        { name = "DiagnosticSignInfo", text = "" },
+        {name = "DiagnosticSignError", text = "" },
+        {name = "DiagnosticSignWarn", text = "" },
+        {name = "DiagnosticSignHint", text = "" },
+        {name = "DiagnosticSignInfo", text = "" },
     }
 
     for _, sign in ipairs(signs) do
@@ -47,20 +47,18 @@ end
 local function lsp_highlight_document(client)
     -- Set autocommands conditional on server_capabilities
     if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec(
-            [[
+        vim.api.nvim_exec([[
             augroup lsp_document_highlight
                 autocmd! * <buffer>
                 autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
                 autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
             augroup END
             ]],
-            false
-        )
+        false)
     end
 end
 
-M.on_attach = function(client, bufnr)
+M.on_attach = function(client, _)
     lsp_highlight_document(client)
 end
 
