01fb6d6097605177ecd1126c2a8fb21cdf654678
orbital <orbital@holgerines.de>
2022-03-16 01:44:50 -0700

    Allow selecting completion options without selecting

--- lua/user/completion.lua
+++ lua/user/completion.lua
@@ -19,6 +19,8 @@ end
 
 cmp.setup {
     mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
         ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
         ["<C-e>"] = cmp.mapping.close(),
