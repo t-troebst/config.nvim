f71669c3e7c745150e25e143ff9bb646554bc964
orbital <orbital@holgerines.de>
2022-08-22 20:45:47 -0700

    Add FixCursorHold plugin for working LSP highlighting.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
     use "wbthomason/packer.nvim" -- Have packer manage itself
     use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
+    use "antoinemadec/FixCursorHold.nvim" -- Fixes a bug in Neovim  with CursorHold
 
     -- Basic editor functionality
     use "nvim-lualine/lualine.nvim" -- Fancy status line
