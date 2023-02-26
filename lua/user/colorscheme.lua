cdaab6e1b1e1e652e749be122d086e7882521302
orbital <orbital@holgerines.de>
2023-02-25 22:18:10 -0800

    Add highlight for C++ attributes

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,13 @@
 -- Colorscheme
 
+local palette_ok, palette = pcall(require, "rose-pine.palette")
+if not palette_ok then return end
+
 vim.cmd [[colorscheme rose-pine]]
 
 -- Custom highlights for DAP
 
 vim.api.nvim_set_hl(0, "Breakpoint", { fg = "#eb6f92" })
 vim.api.nvim_set_hl(0, "Continue", { fg = "#31748f" })
+
+vim.api.nvim_set_hl(0, "@attribute", { fg = palette.muted })
