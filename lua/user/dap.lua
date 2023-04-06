97bd3e6d219eb2d581a5872e1471031f6255d1ec
orbital <orbital@holgerines.de>
2023-04-05 21:24:18 -0700

    Use coroutines + ui.select in dap

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -11,20 +11,20 @@ vim.fn.sign_define("DapLogPoint", { text = '🅻', texthl = 'Logpoint', linehl =
 vim.fn.sign_define("DapBreakpointRejected", { text = '✗', texthl = 'Continue', linehl = '', numhl = 'Breakpoint' })
 
 local function conditional_breakpoint()
-    vim.ui.input({prompt = "Condition: "}, function(value)
+    vim.ui.input({ prompt = "Condition: " }, function(value)
         dap.set_breakpoint(value)
     end)
 end
 
 local function logpoint()
-    vim.ui.input({prompt = "Log: "}, function(log)
+    vim.ui.input({ prompt = "Log: " }, function(log)
         dap.set_breakpoint(nil, nil, log .. "\n")
     end)
 end
 
 local function conditional_logpoint()
-    vim.ui.input({prompt = "Log: "}, function(log)
-        vim.ui.input({prompt = "Condition: "}, function(condition)
+    vim.ui.input({ prompt = "Log: " }, function(log)
+        vim.ui.input({ prompt = "Condition: " }, function(condition)
             dap.set_breakpoint(condition, nil, log .. "\n")
         end)
     end)
@@ -67,19 +67,20 @@ local function select_target(config)
     if not config.targets then
         return nil
     else
-        local targets = {"Choose debug target:"}
+        local coro = coroutine.running()
 
-        for i, t in ipairs(config.targets) do
-            table.insert(targets, tostring(i) .. ". " .. (t.name or t.program))
-        end
+        vim.schedule(function()
+            vim.ui.select(config.targets, {
+                prompt = "Choose target:",
+                format_item = function(target)
+                    return target.name or (target.program .. " " .. table.concat(target.args, " "))
+                end
+            }, function(target)
+                coroutine.resume(coro, target)
+            end)
+        end)
 
-        local idx = vim.fn.inputlist(targets)
-
-        if idx <= 0 or idx > #targets then
-            return nil
-        else
-            return config.targets[idx]
-        end
+        return coroutine.yield()
     end
 end
 
@@ -99,23 +100,32 @@ local cpp_config = {
     runInTerminal = false,
 }
 
-setmetatable(cpp_config, {__call = function(conf)
-    local config = dap_config()
-    local target = select_target(config)
+setmetatable(cpp_config, {
+    __call = function(conf)
+        local config = dap_config()
+        local target = select_target(config)
 
-    local new_conf = vim.deepcopy(conf)
-    new_conf.cwd = config.cwd or "${workspaceFolder}"
+        local new_conf = vim.deepcopy(conf)
+        new_conf.cwd = config.cwd or "${workspaceFolder}"
 
-    if not target then
-        new_conf.program = vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        new_conf.args = {}
-    else
-        new_conf.program = target.program
-        new_conf.args = target.args or {}
+        if not target then
+            local coro = coroutine.running()
+            vim.schedule(function()
+                vim.ui.input({ prompt = "Path to executable:", completion = "file", default = vim.fn.getcwd() .. "/" },
+                    function(path)
+                        coroutine.resume(coro, path)
+                    end)
+            end)
+            new_conf.program = coroutine.yield()
+            new_conf.args = {}
+        else
+            new_conf.program = target.program
+            new_conf.args = target.args or {}
+        end
+
+        return new_conf
     end
-
-    return new_conf
-end})
+})
 
 dap.configurations.cpp = {
     cpp_config
@@ -127,18 +137,17 @@ dap.configurations.rust = dap.configurations.cpp
 -- Python
 
 dap.adapters.python = {
-    type = 'executable';
-    command = 'python';
-    args = { '-m', 'debugpy.adapter' };
+    type = 'executable',
+    command = 'python',
+    args = { '-m', 'debugpy.adapter' },
 }
 
 dap.configurations.python = {
     {
-        type = 'python';
-        request = 'launch';
-        name = "Launch file";
-
-        program = "${file}";
+        type = 'python',
+        request = 'launch',
+        name = "Launch file",
+        program = "${file}",
         pythonPath = function()
             -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
             -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
@@ -151,6 +160,6 @@ dap.configurations.python = {
             else
                 return '/usr/bin/python'
             end
-        end;
+        end,
     },
 }
