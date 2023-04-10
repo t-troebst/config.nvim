a220d5fb025468c067191ca75e5576f072253fac
orbital <orbital@holgerines.de>
2023-04-09 21:34:08 -0700

    Add stylua to null_ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -4,7 +4,8 @@ if not null_ls_ok then return end
 null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.flake8.with(require("user.lsp.settings.flake8")),
-        null_ls.builtins.diagnostics.mypy.with(require("user.lsp.settings.mypy"))
+        null_ls.builtins.diagnostics.mypy.with(require("user.lsp.settings.mypy")),
+        null_ls.builtins.formatting.stylua.with({})
     },
     log_level = "debug"
 })
