8a75f0312528eff1f5640eed8fbaa5c4b42b2537
orbital <orbital@holgerines.de>
2023-04-11 20:04:24 -0700

    Reenable dressing input

--- lua/user/dressing.lua
+++ lua/user/dressing.lua
@@ -1,6 +1,7 @@
 local dressing = require("dressing")
 
 dressing.setup {
-    -- Doesn't work too well for me...
-    input = { enabled = false },
+    input = {
+        relative = "editor"
+    }
 }
