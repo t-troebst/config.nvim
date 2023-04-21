e6c5358f6f650e0edac1a8a3925544a6d769927c
orbital <orbital@holgerines.de>
2023-04-20 23:52:47 -0700

    Add checkThirdParty = false to lua ls settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -96,6 +96,7 @@ return {
                                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                                     [vim.fn.stdpath("config") .. "/lua"] = true,
                                 },
+                                checkThirdParty = false,
                             },
                         },
                     },
