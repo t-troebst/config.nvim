d73d7abec5672461267ddfef469a1ca4f8e6bb9d
orbital <orbital@holgerines.de>
2023-05-14 14:39:34 -0700

    Remove unused nvim-tree mappings

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -35,14 +35,6 @@ local function setup()
             width = 30,
             hide_root_folder = false,
             side = "right",
-            mappings = {
-                custom_only = false,
-                list = {
-                    { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-                    { key = "h", cb = tree_cb("close_node") },
-                    { key = "v", cb = tree_cb("vsplit") },
-                },
-            },
             number = false,
             relativenumber = false,
         },
