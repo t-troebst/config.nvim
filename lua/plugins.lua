89b289b0e50f4845ca714896f92f599d92c4d1db
Thorben Troebst <thorben.troebst@gmail.com>
2026-01-17 21:12:07 +0000

    Add perfanno

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -109,4 +109,27 @@ return {
             })
         end,
     },
+
+    {
+        "t-troebst/perfanno.nvim",
+        config = function()
+            local perfanno = require("perfanno")
+            local util = require("perfanno.util")
+            local palette = require("rose-pine.palette")
+
+            perfanno.setup {
+                line_highlights = util.make_bg_highlights(nil, palette.love, 10),
+                vt_highlight = util.make_fg_highlight(palette.love),
+            }
+        end,
+        dev = true,
+        cmd = {
+            "PerfCacheDelete",
+            "PerfCacheLoad",
+            "PerfLoadCallGraph",
+            "PerfLoadFlamegraph",
+            "PerfLoadFlat",
+            "PerfLuaProfileStart",
+        },
+    },
 }
