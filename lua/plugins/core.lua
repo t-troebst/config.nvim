5ac1c4bbbaa2b0d6b0478e6467a06afff6065a01
Thorben Troebst <thorben.troebst@gmail.com>
2026-01-17 16:28:14 +0000

    Make config more consistent

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,8 +1,8 @@
 return {
     { "nvim-tree/nvim-web-devicons", lazy = true },
     { "nvim-lua/plenary.nvim", lazy = true },
-    "folke/which-key.nvim",
-    "cbochs/grapple.nvim",
+    { "folke/which-key.nvim" },
+    { "cbochs/grapple.nvim" },
     { "phaazon/hop.nvim", config = true, lazy = true, cmd = { "HopChar1" } },
     { "numToStr/Comment.nvim", config = true },
     { "lewis6991/gitsigns.nvim", config = true },
